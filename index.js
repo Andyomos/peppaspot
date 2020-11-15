72960fac74bd10c81f6a14021735a1c7ffb35e73
Anderson Omoragbon <61177055+Andyomos@users.noreply.github.com>
2020-11-14 17:52:41 -0700

    Changed files

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems);
 
     // control icture zoom
     let elem = document.querySelectorAll('.materialboxed');
