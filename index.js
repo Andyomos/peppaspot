47d27c33ccf225960d5a8afe51ed588a940b39e1
Anderson Omoragbon <61177055+Andyomos@users.noreply.github.com>
2021-02-07 01:20:30 -0700

    Added pictures for the new picture slider

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 let myDate = new Date();
+// Portfolio
+const nextBtn = document.getElementsByClassName('nextBtn');
+const prevBtn = document.getElementsByClassName('prevBtn');
+const imagesContainer = document.querySelector('.images');
+let counter = 0;
+
+
+const pictures = [
+  `url(image/showcaseimg/showcase-${0}.jpg)`,
+  `url(image/showcaseimg/showcase-${1}.jpg)`,
+  `image/showcaseimg/showcase-${2}.jpg)`,
+  `url(image/showcaseimg/showcase-${3}.jpg)`,
+  `url(image/showcaseimg/showcase-${4}.jpg)`,
+  `url(image/showcaseimg/showcase-${5}.jpg)`,
+  `url(image/showcaseimg/showcase-${6}.jpg)`,
+];
+
+// Portfolio Picture Slide
+const nextSlide = () => {
+    if (counter === pictures.length) {
+        counter = 0;
+    }
+        
+    imagesContainer.style.backgroundImage = `url(image/showcaseimg/showcase-${counter}.jpg)`;
+    counter++;  
+}
+
+const prevSlide = () => {
+    if (counter === pictures.length) {   
+        counter = 0;
+    }
+    imagesContainer.style.backgroundImage = `url(image/showcaseimg/showcase-${counter}.jpg)`;
+    counter++;
+} 
 
 
 document.addEventListener('DOMContentLoaded', function() {
